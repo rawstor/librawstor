a1a94832f12ef0e3238b4117ec2de2ffb0e32bf7
vasily <vasily.stepanov@gmail.com>
2025-01-03 19:49:45 +0300

    add: readv and writev api functions

--- src/rawstor.c
+++ src/rawstor.c
@@ -13,10 +13,52 @@ void rawstor_free(RawstorDevice *device) {
     free(device);
 }
 
-void rawstor_read(RawstorDevice *device, void *buf, size_t size, size_t offset) {
+
+void rawstor_read(
+  RawstorDevice *device,
+  size_t offset, size_t size,
+  void *buf
+) {
     memcpy(buf, device + offset, size);
 }
 
-void rawstor_write(RawstorDevice *device, const void *buf, size_t size, size_t offset) {
+
+void rawstor_readv(
+  RawstorDevice *device,
+  size_t offset, size_t size,
+  struct iovec *iov, unsigned int niov
+) {
+    for (unsigned int i = 0; i < niov; ++i) {
+        size_t chunk_size = size < iov[i].iov_len ? size : iov[i].iov_len;
+
+        rawstor_read(device, offset, chunk_size, iov[i].iov_base);
+
+        size -= chunk_size;
+        offset += chunk_size;
+    }
+}
+
+
+void rawstor_write(
+  RawstorDevice *device,
+  size_t offset, size_t size,
+  const void *buf
+) {
     memcpy(device + offset, buf, size);
+}
+
+
+void rawstor_writev(
+  RawstorDevice *device,
+  size_t offset, size_t size,
+  const struct iovec *iov, unsigned int niov
+) {
+    for (unsigned int i = 0; i < niov; ++i) {
+        size_t chunk_size = size < iov[i].iov_len ? size : iov[i].iov_len;
+
+        rawstor_write(device, offset, chunk_size, iov[i].iov_base);
+
+        size -= chunk_size;
+        offset += chunk_size;
+    }
 }
--- src/rawstor.h
+++ src/rawstor.h
@@ -2,6 +2,7 @@
 #define _RAWSTOR_H_
 
 #include <stddef.h>
+#include <sys/uio.h>
 
 
 typedef void RawstorDevice;
@@ -11,9 +12,29 @@ RawstorDevice* rawstor_alloc(size_t size);
 
 void rawstor_free(RawstorDevice *device);
 
-void rawstor_read(RawstorDevice *device, void *buf, size_t size, size_t offset);
+void rawstor_read(
+  RawstorDevice *device,
+  size_t offset, size_t size,
+  void *buf
+);
 
-void rawstor_write(RawstorDevice *device, const void *buf, size_t size, size_t offset);
+void rawstor_readv(
+  RawstorDevice *device,
+  size_t offset, size_t size,
+  struct iovec *iov, unsigned int niov
+);
+
+void rawstor_write(
+  RawstorDevice *device,
+  size_t offset, size_t size,
+  const void *buf
+);
+
+void rawstor_writev(
+  RawstorDevice *device,
+  size_t offset, size_t size,
+  const struct iovec *iov, unsigned int niov
+);
 
 
 #endif // _RAWSTOR_H_
