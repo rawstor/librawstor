47047be665c9bd36a36e098b6fb3e517e3267b5a
vasily <vasily.stepanov@gmail.com>
2025-01-13 18:36:23 +0000

    ref: req renamed to request

--- vu/server.c
+++ vu/server.c
@@ -16,38 +16,68 @@
 #define QUEUE_DEPTH 256
 
 #define EVENT_TYPE_ACCEPT 0
+#define EVENT_TYPE_READ 1
+
+#define READ_SZ 8192
 
 
 typedef struct {
     int event_type;
-    int session;
+    int client_socket;
+    struct iovec iov[1];
 } Request;
 
 
 #ifdef ENABLE_LIBURING
-static int add_accept_request(struct io_uring *ring, int s) {
-    Request *req = malloc(sizeof(Request));
-    if (req == NULL) {
+static int push_accept_request(struct io_uring *ring, int server_socket) {
+    Request *request = malloc(sizeof(Request));
+    if (request == NULL) {
         perror("malloc() failed:");
         return -1;
     }
-    req->event_type = EVENT_TYPE_ACCEPT;
+    request->event_type = EVENT_TYPE_ACCEPT;
 
     struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
-    io_uring_prep_accept(sqe, s, NULL, NULL, 0);
-
-    io_uring_sqe_set_data(sqe, req);
+    io_uring_prep_accept(sqe, server_socket, NULL, NULL, 0);
+    io_uring_sqe_set_data(sqe, request);
     io_uring_submit(ring);
     return 0;
 }
 
 
-static int server_loop(int s) {
+static int push_read_request(struct io_uring *ring, int client_socket) {
+    Request *request = malloc(sizeof(Request));
+    if (request == NULL) {
+        perror("malloc() failed:");
+        return -1;
+    }
+    request->iov[0].iov_base = malloc(READ_SZ);
+    memset(request->iov[0].iov_base, 0, READ_SZ);
+    request->iov[0].iov_len = READ_SZ;
+    request->event_type = EVENT_TYPE_READ;
+    request->client_socket = client_socket;
+
+    struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
+    io_uring_prep_readv(sqe, client_socket, &request->iov[0], 1, 0);
+    io_uring_sqe_set_data(sqe, request);
+    io_uring_submit(ring);
+    return 0;
+}
+
+
+static int handle_client_request(Request *request) {
+    (void)(request);
+    printf("parse client request goes here\n");
+    return 0;
+}
+
+
+static int server_loop(int server_socket) {
     int rval;
     struct io_uring ring;
 
     rval = io_uring_queue_init(QUEUE_DEPTH, &ring, 0);
-    if (rval) {
+    if (rval < 0) {
         fprintf(
             stderr,
             "io_uring_queue_init() failed: %s\n",
@@ -55,7 +85,7 @@ static int server_loop(int s) {
         return -1;
     };
 
-    if (add_accept_request(&ring, s)) {
+    if (push_accept_request(&ring, server_socket)) {
         io_uring_queue_exit(&ring);
         return -1;
     }
@@ -64,7 +94,7 @@ static int server_loop(int s) {
     while (1) {
         printf("Waiting for event...\n");
         rval = io_uring_wait_cqe(&ring, &cqe);
-        if (rval) {
+        if (rval < 0) {
             fprintf(
                 stderr,
                 "io_uring_wait_cqe() failed: %s\n",
@@ -76,41 +106,48 @@ static int server_loop(int s) {
             continue;
         }
 
-        Request *req = (Request*)cqe->user_data;
-        printf("Event received: %d\n", req->event_type);
+        Request *request = (Request*)cqe->user_data;
+        printf("Event received: %d\n", request->event_type);
         if (cqe->res < 0) {
             fprintf(
                 stderr,
                 "io_uring request failed for event %d: %s\n",
-                req->event_type,
+                request->event_type,
                 strerror(-cqe->res));
-            free(req);
+            free(request);
             io_uring_cqe_seen(&ring, cqe);
             // TODO: Fatal error here?
             continue;
         }
 
-        switch (req->event_type) {
+        switch (request->event_type) {
             case EVENT_TYPE_ACCEPT:
-                if(close(cqe->res)) {
-                    perror("close() failed");
-                }
-                free(req);
-                if (add_accept_request(&ring, s)) {
+                if (push_accept_request(&ring, server_socket)) {
                     // TODO: free() other requests in queue.
+                    free(request);
                     io_uring_cqe_seen(&ring, cqe);
                     io_uring_queue_exit(&ring);
                     return -1;
                 }
+                if (push_read_request(&ring, cqe->res)) {
+                    break;
+                }
+                break;
+            case EVENT_TYPE_READ:
+                handle_client_request(request);
+                if(close(request->client_socket)) {
+                    perror("close() failed");
+                }
+                free(request->iov[0].iov_base);
                 break;
             default:
                 fprintf(
                     stderr,
                     "Unexpected event type: %d\n",
-                    req->event_type);
-                free(req);
+                    request->event_type);
                 break;
         }
+        free(request);
 
         io_uring_cqe_seen(&ring, cqe);
     }
@@ -128,8 +165,8 @@ int rawstor_vu_server(
 {
     (void)(object_id);
 
-    int s = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (s < 0) {
+    int server_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (server_socket < 0) {
         perror("socket() failed");
         return -1;
     }
@@ -139,20 +176,20 @@ int rawstor_vu_server(
     };
     strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path) - 1);
 
-    if (bind(s, (struct sockaddr *)&addr, sizeof(addr))) {
+    if (bind(server_socket, (struct sockaddr *)&addr, sizeof(addr))) {
         perror("bind() failed");
-        if (close(s)) {
+        if (close(server_socket)) {
             err(EXIT_FAILURE, NULL);
         }
         return -1;
     }
 
-    if (listen(s, 1)) {
+    if (listen(server_socket, 1)) {
         perror("listen() failed");
         if (unlink(addr.sun_path)) {
             err(EXIT_FAILURE, NULL);
         }
-        if (close(s)) {
+        if (close(server_socket)) {
             err(EXIT_FAILURE, NULL);
         }
         return -1;
@@ -160,18 +197,18 @@ int rawstor_vu_server(
 
     int rval = 0;
 #ifdef ENABLE_LIBURING
-    rval = server_loop(s);
+    rval = server_loop(server_socket);
 #endif
 
     if (unlink(socket_path)) {
         perror("unlink() failed");
-        if (close(s)) {
+        if (close(server_socket)) {
             err(EXIT_FAILURE, NULL);
         }
         return -1;
     }
 
-    if (close(s)) {
+    if (close(server_socket)) {
         perror("close() failed");
         return -1;
     }
