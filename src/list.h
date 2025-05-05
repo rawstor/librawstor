91fa5ad757898d21ab2bfbbe3ef69615116ec989
vasily <vasily.stepanov@gmail.com>
2025-05-06 00:07:13 +0300

    add: rawstor list

---
+++ src/list.h
@@ -0,0 +1,28 @@
+#ifndef RAWSTOR_LIST_H
+#define RAWSTOR_LIST_H
+
+#include <stddef.h>
+
+
+typedef struct RawstorList RawstorList;
+
+
+RawstorList* rawstor_list_create(size_t object_size);
+
+void rawstor_list_delete(RawstorList *list);
+
+void* rawstor_list_iter(RawstorList *list);
+
+void* rawstor_list_next(void *iter);
+
+void* rawstor_list_append(RawstorList *list);
+
+void* rawstor_list_remove(RawstorList *list, void *iter);
+
+int rawstor_list_empty(RawstorList *list);
+
+size_t rawstor_list_size(RawstorList *list);
+
+
+
+#endif // RAWSTOR_LIST_H
