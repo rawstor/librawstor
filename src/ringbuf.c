dbebc9ed02eb414bc6ca03587eb2ce712f9deb86
vasily <vasily.stepanov@gmail.com>
2025-05-08 23:48:41 +0300

    add: rawstor ringbuf

---
+++ src/ringbuf.c
@@ -0,0 +1,89 @@
+#include "ringbuf.h"
+
+#include <errno.h>
+#include <stdlib.h>
+
+
+struct RawstorRingBuf {
+    void *data;
+    size_t capacity;
+    size_t object_size;
+    size_t head;
+    size_t tail;
+};
+
+
+RawstorRingBuf* rawstor_ringbuf_create(size_t capacity, size_t object_size) {
+    if (capacity == 0) {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    RawstorRingBuf *buf = malloc(sizeof(RawstorRingBuf));
+    if (buf == NULL) {
+        return NULL;
+    }
+
+    buf->capacity = capacity + 1;
+    buf->object_size = object_size;
+    buf->head = 0;
+    buf->tail = 0;
+    buf->data = calloc(buf->capacity, object_size);
+    if (buf->data == NULL) {
+        free(buf);
+        return NULL;
+    }
+
+    return buf;
+}
+
+
+void rawstor_ringbuf_delete(RawstorRingBuf *buf) {
+    free(buf->data);
+    free(buf);
+}
+
+
+void* rawstor_ringbuf_head(RawstorRingBuf *buf) {
+    return buf->data + buf->head * buf->object_size;
+}
+
+
+void* rawstor_ringbuf_tail(RawstorRingBuf *buf) {
+    return buf->data + buf->tail * buf->object_size;
+}
+
+
+int rawstor_ringbuf_push(RawstorRingBuf *buf) {
+    size_t next = (buf->head + 1) % buf->capacity;
+    if (next == buf->tail) {
+        errno = ENOBUFS;
+        return -errno;
+    }
+
+    buf->head = next;
+
+    return 0;
+}
+
+
+int rawstor_ringbuf_pop(RawstorRingBuf *buf) {
+    if (buf->tail == buf->head) {
+        errno = ENOBUFS;
+        return -errno;
+    }
+
+    buf->tail = (buf->tail + 1) % buf->capacity;
+
+    return 0;
+}
+
+
+int rawstor_ringbuf_empty(RawstorRingBuf *buf) {
+    return buf->tail == buf->head;
+}
+
+
+size_t rawstor_ringbuf_size(RawstorRingBuf *buf) {
+    return (buf->capacity + buf->head - buf->tail) % buf->capacity;
+}
---
+++ tests/test_ringbuf.c
@@ -0,0 +1,116 @@
+#include "ringbuf.h"
+
+#include "utils.h"
+
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+static int test_ringbuf_empty() {
+    RawstorRingBuf *buf = rawstor_ringbuf_create(3, sizeof(int));
+
+    assertTrue(rawstor_ringbuf_pop(buf) == -ENOBUFS);
+    assertTrue(errno == ENOBUFS);
+
+    assertTrue(rawstor_ringbuf_empty(buf) != 0);
+    assertTrue(rawstor_ringbuf_size(buf) == 0);
+
+    rawstor_ringbuf_delete(buf);
+    return 0;
+}
+
+
+static int test_ringbuf_invalid() {
+    RawstorRingBuf *buf = rawstor_ringbuf_create(0, sizeof(int));
+
+    assertTrue(buf == NULL);
+    assertTrue(errno == EINVAL);
+
+    return 0;
+}
+
+
+static int test_ringbuf_basics() {
+    RawstorRingBuf *buf = rawstor_ringbuf_create(3, sizeof(int));
+
+    assertTrue(rawstor_ringbuf_empty(buf) != 0);
+
+    *(int*)rawstor_ringbuf_head(buf) = 1;
+    assertTrue(rawstor_ringbuf_push(buf) == 0);
+    assertTrue(rawstor_ringbuf_empty(buf) == 0);
+
+    *(int*)rawstor_ringbuf_head(buf) = 2;
+    assertTrue(rawstor_ringbuf_push(buf) == 0);
+    assertTrue(rawstor_ringbuf_empty(buf) == 0);
+
+    *(int*)rawstor_ringbuf_head(buf) = 3;
+    assertTrue(rawstor_ringbuf_push(buf) == 0);
+    assertTrue(rawstor_ringbuf_empty(buf) == 0);
+
+    assertTrue(rawstor_ringbuf_push(buf) == -ENOBUFS);
+    assertTrue(errno == ENOBUFS);
+
+    assertTrue(*(int*)rawstor_ringbuf_tail(buf) == 1);
+    assertTrue(rawstor_ringbuf_pop(buf) == 0);
+    assertTrue(rawstor_ringbuf_empty(buf) == 0);
+
+    assertTrue(*(int*)rawstor_ringbuf_tail(buf) == 2);
+    assertTrue(rawstor_ringbuf_pop(buf) == 0);
+    assertTrue(rawstor_ringbuf_empty(buf) == 0);
+
+    assertTrue(*(int*)rawstor_ringbuf_tail(buf) == 3);
+    assertTrue(rawstor_ringbuf_pop(buf) == 0);
+    assertTrue(rawstor_ringbuf_empty(buf) != 0);
+
+    assertTrue(rawstor_ringbuf_pop(buf) == -ENOBUFS);
+    assertTrue(errno == ENOBUFS);
+
+    rawstor_ringbuf_delete(buf);
+    return 0;
+}
+
+
+static int test_ringbuf_overlap() {
+    RawstorRingBuf *buf = rawstor_ringbuf_create(4, sizeof(int));
+
+    assertTrue(rawstor_ringbuf_size(buf) == 0);
+
+    assertTrue(rawstor_ringbuf_push(buf) == 0);
+    assertTrue(rawstor_ringbuf_size(buf) == 1);
+
+    assertTrue(rawstor_ringbuf_push(buf) == 0);
+    assertTrue(rawstor_ringbuf_size(buf) == 2);
+
+    assertTrue(rawstor_ringbuf_push(buf) == 0);
+    assertTrue(rawstor_ringbuf_size(buf) == 3);
+
+    assertTrue(rawstor_ringbuf_push(buf) == 0);
+    assertTrue(rawstor_ringbuf_size(buf) == 4);
+
+    assertTrue(rawstor_ringbuf_push(buf) != 0);
+    assertTrue(rawstor_ringbuf_size(buf) == 4);
+
+    assertTrue(rawstor_ringbuf_pop(buf) == 0);
+    assertTrue(rawstor_ringbuf_size(buf) == 3);
+    assertTrue(rawstor_ringbuf_push(buf) == 0);
+    assertTrue(rawstor_ringbuf_size(buf) == 4);
+
+    assertTrue(rawstor_ringbuf_pop(buf) == 0);
+    assertTrue(rawstor_ringbuf_size(buf) == 3);
+    assertTrue(rawstor_ringbuf_push(buf) == 0);
+    assertTrue(rawstor_ringbuf_size(buf) == 4);
+
+    rawstor_ringbuf_delete(buf);
+    return 0;
+}
+
+
+int main() {
+    int rval = 0;
+    rval += test_ringbuf_empty();
+    rval += test_ringbuf_invalid();
+    rval += test_ringbuf_basics();
+    rval += test_ringbuf_overlap();
+    return rval ? EXIT_FAILURE : EXIT_SUCCESS;
+}
