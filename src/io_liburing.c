bd5315ad8d3e332d73986fb1a83350454c4a70e3
vasily <vasily.stepanov@gmail.com>
2025-07-28 18:17:32 +0300

    ref: simplify io_uring code

--- src/io_liburing.c
+++ src/io_liburing.c
@@ -20,6 +20,7 @@ struct RawstorIOEvent {
     RawstorIOCallback *callback;
 
     size_t size;
+    struct io_uring_sqe *sqe;
     struct io_uring_cqe *cqe;
 
     void *data;
@@ -38,6 +39,42 @@ struct RawstorIO {
 
 
 const char* rawstor_io_engine_name = "liburing";
+
+
+static inline RawstorIOEvent* rawstor_io_create_event(
+    RawstorIO *io,
+    int fd, size_t size,
+    RawstorIOCallback *cb, void *data)
+{
+    /**
+     * TODO: Since mempool count is equal to sqe count,
+     * do we really have to have this check?
+     */
+    if (rawstor_mempool_available(io->events_pool) == 0) {
+        errno = ENOBUFS;
+        return NULL;
+    }
+
+    struct io_uring_sqe *sqe = io_uring_get_sqe(&io->ring);
+    if (sqe == NULL) {
+        errno = ENOBUFS;
+        return NULL;
+    }
+
+    RawstorIOEvent *event = rawstor_mempool_alloc(io->events_pool);
+    *event = (RawstorIOEvent) {
+        .fd = fd,
+        .callback = cb,
+        .size = size,
+        .sqe = sqe,
+        // .cqe
+        .data = data,
+    };
+
+    io_uring_sqe_set_data(sqe, event);
+
+    return event;
+}
 
 
 RawstorIO* rawstor_io_create(unsigned int depth) {
@@ -94,36 +131,17 @@ int rawstor_io_read(
     int fd, void *buf, size_t size,
     RawstorIOCallback *cb, void *data)
 {
-    /**
-     * TODO: Since mempool count is equal to sqe count,
-     * do we really have to have this check?
-     */
-    if (rawstor_mempool_available(io->events_pool) == 0) {
-        errno = ENOBUFS;
+    RawstorIOEvent *event = rawstor_io_create_event(io, fd, size, cb, data);
+    if (event == NULL) {
         return -errno;
     }
 
-    struct io_uring_sqe *sqe = io_uring_get_sqe(&io->ring);
-    if (sqe == NULL) {
-        errno = ENOBUFS;
-        return -errno;
-    }
+    io_uring_prep_read(event->sqe, fd, buf, size, 0);
 
-    RawstorIOEvent *event = rawstor_mempool_alloc(io->events_pool);
-    *event = (RawstorIOEvent) {
-        .fd = fd,
-        .callback = cb,
-        .size = size,
-        // .cqe
-        .data = data,
 #ifdef RAWSTOR_TRACE_EVENTS
-        .trace_event = rawstor_trace_event_begin(
-            "read(%d, %zu)\n", fd, size),
+    event->trace_event = rawstor_trace_event_begin(
+        "read(%d, %zu)\n", fd, size);
 #endif
-    };
-
-    io_uring_prep_read(sqe, fd, buf, size, 0);
-    io_uring_sqe_set_data(sqe, event);
 
     return 0;
 }
@@ -134,36 +152,17 @@ int rawstor_io_pread(
     int fd, void *buf, size_t size, off_t offset,
     RawstorIOCallback *cb, void *data)
 {
-    /**
-     * TODO: Since mempool count is equal to sqe count,
-     * do we really have to have this check?
-     */
-    if (rawstor_mempool_available(io->events_pool) == 0) {
-        errno = ENOBUFS;
+    RawstorIOEvent *event = rawstor_io_create_event(io, fd, size, cb, data);
+    if (event == NULL) {
         return -errno;
     }
 
-    struct io_uring_sqe *sqe = io_uring_get_sqe(&io->ring);
-    if (sqe == NULL) {
-        errno = ENOBUFS;
-        return -errno;
-    }
+    io_uring_prep_read(event->sqe, fd, buf, size, offset);
 
-    RawstorIOEvent *event = rawstor_mempool_alloc(io->events_pool);
-    *event = (RawstorIOEvent) {
-        .fd = fd,
-        .callback = cb,
-        .size = size,
-        // .cqe
-        .data = data,
 #ifdef RAWSTOR_TRACE_EVENTS
-        .trace_event = rawstor_trace_event_begin(
-            "pread(%d, %zu)\n", fd, size),
+    event->trace_event = rawstor_trace_event_begin(
+        "pread(%d, %zu)\n", fd, size);
 #endif
-    };
-
-    io_uring_prep_read(sqe, fd, buf, size, offset);
-    io_uring_sqe_set_data(sqe, event);
 
     return 0;
 }
@@ -174,36 +173,17 @@ int rawstor_io_readv(
     int fd, struct iovec *iov, unsigned int niov, size_t size,
     RawstorIOCallback *cb, void *data)
 {
-    /**
-     * TODO: Since mempool count is equal to sqe count,
-     * do we really have to have this check?
-     */
-    if (rawstor_mempool_available(io->events_pool) == 0) {
-        errno = ENOBUFS;
+    RawstorIOEvent *event = rawstor_io_create_event(io, fd, size, cb, data);
+    if (event == NULL) {
         return -errno;
     }
 
-    struct io_uring_sqe *sqe = io_uring_get_sqe(&io->ring);
-    if (sqe == NULL) {
-        errno = ENOBUFS;
-        return -errno;
-    }
+    io_uring_prep_readv(event->sqe, fd, iov, niov, 0);
 
-    RawstorIOEvent *event = rawstor_mempool_alloc(io->events_pool);
-    *event = (RawstorIOEvent) {
-        .fd = fd,
-        .callback = cb,
-        .size = size,
-        // .cqe
-        .data = data,
 #ifdef RAWSTOR_TRACE_EVENTS
-        .trace_event = rawstor_trace_event_begin(
-            "readv(%d, %zu)\n", fd, size),
+    event->trace_event = rawstor_trace_event_begin(
+        "readv(%d, %zu)\n", fd, size);
 #endif
-    };
-
-    io_uring_prep_readv(sqe, fd, iov, niov, 0);
-    io_uring_sqe_set_data(sqe, event);
 
     return 0;
 }
@@ -214,36 +194,17 @@ int rawstor_io_preadv(
     int fd, struct iovec *iov, unsigned int niov, size_t size, off_t offset,
     RawstorIOCallback *cb, void *data)
 {
-    /**
-     * TODO: Since mempool count is equal to sqe count,
-     * do we really have to have this check?
-     */
-    if (rawstor_mempool_available(io->events_pool) == 0) {
-        errno = ENOBUFS;
+    RawstorIOEvent *event = rawstor_io_create_event(io, fd, size, cb, data);
+    if (event == NULL) {
         return -errno;
     }
 
-    struct io_uring_sqe *sqe = io_uring_get_sqe(&io->ring);
-    if (sqe == NULL) {
-        errno = ENOBUFS;
-        return -errno;
-    }
+    io_uring_prep_readv(event->sqe, fd, iov, niov, offset);
 
-    RawstorIOEvent *event = rawstor_mempool_alloc(io->events_pool);
-    *event = (RawstorIOEvent) {
-        .fd = fd,
-        .callback = cb,
-        .size = size,
-        // .cqe
-        .data = data,
 #ifdef RAWSTOR_TRACE_EVENTS
-        .trace_event = rawstor_trace_event_begin(
-            "preadv(%d, %zu)\n", fd, size),
+    event->trace_event = rawstor_trace_event_begin(
+        "preadv(%d, %zu)\n", fd, size);
 #endif
-    };
-
-    io_uring_prep_readv(sqe, fd, iov, niov, offset);
-    io_uring_sqe_set_data(sqe, event);
 
     return 0;
 }
@@ -254,36 +215,17 @@ int rawstor_io_write(
     int fd, void *buf, size_t size,
     RawstorIOCallback *cb, void *data)
 {
-    /**
-     * TODO: Since mempool count is equal to sqe count,
-     * do we really have to have this check?
-     */
-    if (rawstor_mempool_available(io->events_pool) == 0) {
-        errno = ENOBUFS;
+    RawstorIOEvent *event = rawstor_io_create_event(io, fd, size, cb, data);
+    if (event == NULL) {
         return -errno;
     }
 
-    struct io_uring_sqe *sqe = io_uring_get_sqe(&io->ring);
-    if (sqe == NULL) {
-        errno = ENOBUFS;
-        return -errno;
-    }
+    io_uring_prep_write(event->sqe, fd, buf, size, 0);
 
-    RawstorIOEvent *event = rawstor_mempool_alloc(io->events_pool);
-    *event = (RawstorIOEvent) {
-        .fd = fd,
-        .callback = cb,
-        .size = size,
-        // .cqe
-        .data = data,
 #ifdef RAWSTOR_TRACE_EVENTS
-        .trace_event = rawstor_trace_event_begin(
-            "write(%d, %zu)\n", fd, size),
+    event->trace_event = rawstor_trace_event_begin(
+        "write(%d, %zu)\n", fd, size);
 #endif
-    };
-
-    io_uring_prep_write(sqe, fd, buf, size, 0);
-    io_uring_sqe_set_data(sqe, event);
 
     return 0;
 }
@@ -294,36 +236,17 @@ int rawstor_io_pwrite(
     int fd, void *buf, size_t size, off_t offset,
     RawstorIOCallback *cb, void *data)
 {
-    /**
-     * TODO: Since mempool count is equal to sqe count,
-     * do we really have to have this check?
-     */
-    if (rawstor_mempool_available(io->events_pool) == 0) {
-        errno = ENOBUFS;
+    RawstorIOEvent *event = rawstor_io_create_event(io, fd, size, cb, data);
+    if (event == NULL) {
         return -errno;
     }
 
-    struct io_uring_sqe *sqe = io_uring_get_sqe(&io->ring);
-    if (sqe == NULL) {
-        errno = ENOBUFS;
-        return -errno;
-    }
+    io_uring_prep_write(event->sqe, fd, buf, size, offset);
 
-    RawstorIOEvent *event = rawstor_mempool_alloc(io->events_pool);
-    *event = (RawstorIOEvent) {
-        .fd = fd,
-        .callback = cb,
-        .size = size,
-        // .cqe
-        .data = data,
 #ifdef RAWSTOR_TRACE_EVENTS
-        .trace_event = rawstor_trace_event_begin(
-            "pwrite(%d, %zu)\n", fd, size),
+    event->trace_event = rawstor_trace_event_begin(
+        "pwrite(%d, %zu)\n", fd, size);
 #endif
-    };
-
-    io_uring_prep_write(sqe, fd, buf, size, offset);
-    io_uring_sqe_set_data(sqe, event);
 
     return 0;
 }
@@ -334,36 +257,17 @@ int rawstor_io_writev(
     int fd, struct iovec *iov, unsigned int niov, size_t size,
     RawstorIOCallback *cb, void *data)
 {
-    /**
-     * TODO: Since mempool count is equal to sqe count,
-     * do we really have to have this check?
-     */
-    if (rawstor_mempool_available(io->events_pool) == 0) {
-        errno = ENOBUFS;
+    RawstorIOEvent *event = rawstor_io_create_event(io, fd, size, cb, data);
+    if (event == NULL) {
         return -errno;
     }
 
-    struct io_uring_sqe *sqe = io_uring_get_sqe(&io->ring);
-    if (sqe == NULL) {
-        errno = ENOBUFS;
-        return -errno;
-    }
+    io_uring_prep_writev(event->sqe, fd, iov, niov, 0);
 
-    RawstorIOEvent *event = rawstor_mempool_alloc(io->events_pool);
-    *event = (RawstorIOEvent) {
-        .fd = fd,
-        .callback = cb,
-        .size = size,
-        // .cqe
-        .data = data,
 #ifdef RAWSTOR_TRACE_EVENTS
-        .trace_event = rawstor_trace_event_begin(
-            "writev(%d, %zu)\n", fd, size),
+    event->trace_event = rawstor_trace_event_begin(
+        "writev(%d, %zu)\n", fd, size);
 #endif
-    };
-
-    io_uring_prep_writev(sqe, fd, iov, niov, 0);
-    io_uring_sqe_set_data(sqe, event);
 
     return 0;
 }
@@ -374,36 +278,17 @@ int rawstor_io_pwritev(
     int fd, struct iovec *iov, unsigned int niov, size_t size, off_t offset,
     RawstorIOCallback *cb, void *data)
 {
-    /**
-     * TODO: Since mempool count is equal to sqe count,
-     * do we really have to have this check?
-     */
-    if (rawstor_mempool_available(io->events_pool) == 0) {
-        errno = ENOBUFS;
+    RawstorIOEvent *event = rawstor_io_create_event(io, fd, size, cb, data);
+    if (event == NULL) {
         return -errno;
     }
 
-    struct io_uring_sqe *sqe = io_uring_get_sqe(&io->ring);
-    if (sqe == NULL) {
-        errno = ENOBUFS;
-        return -errno;
-    }
+    io_uring_prep_writev(event->sqe, fd, iov, niov, offset);
 
-    RawstorIOEvent *event = rawstor_mempool_alloc(io->events_pool);
-    *event = (RawstorIOEvent) {
-        .fd = fd,
-        .callback = cb,
-        .size = size,
-        // .cqe
-        .data = data,
 #ifdef RAWSTOR_TRACE_EVENTS
-        .trace_event = rawstor_trace_event_begin(
-            "pwritev(%d, %zu)\n", fd, size),
+    event->trace_event = rawstor_trace_event_begin(
+        "pwritev(%d, %zu)\n", fd, size);
 #endif
-    };
-
-    io_uring_prep_writev(sqe, fd, iov, niov, offset);
-    io_uring_sqe_set_data(sqe, event);
 
     return 0;
 }
