e346840863fa320dcb08fc20b0b5f3c8892bf97d
vasily <vasily.stepanov@gmail.com>
2025-07-30 00:12:16 +0300

    add: tests

---
+++ tests/test_threading.c
@@ -0,0 +1,116 @@
+#include "threading.h"
+
+#include "utils.h"
+
+#include <stddef.h>
+#include <stdlib.h>
+
+
+struct TestThreadingContext {
+    RawstorMutex *mutex;
+    RawstorCond *cond;
+
+    int wait;
+    int value;
+};
+
+
+static void* test_thread(void *data) {
+    struct TestThreadingContext *context = data;
+
+    rawstor_mutex_lock(context->mutex);
+    ++context->wait;
+    rawstor_cond_wait(context->cond, context->mutex);
+    --context->wait;
+    ++context->value;
+    rawstor_mutex_unlock(context->mutex);
+
+    return context;
+}
+
+
+static int test_cond_signal() {
+    struct TestThreadingContext context = (struct TestThreadingContext) {
+        .mutex = rawstor_mutex_create(),
+        .cond = rawstor_cond_create(),
+        .wait = 0,
+        .value = 0,
+    };
+
+    assertTrue(context.mutex != NULL);
+    assertTrue(context.cond != NULL);
+
+    RawstorThread *thread = rawstor_thread_create(test_thread, &context);
+
+    while (1) {
+        rawstor_mutex_lock(context.mutex);
+        int wait = context.wait;
+        rawstor_mutex_unlock(context.mutex);
+        if (wait == 1) {
+            break;
+        }
+    }
+
+    rawstor_mutex_lock(context.mutex);
+    rawstor_cond_signal(context.cond);
+    rawstor_mutex_unlock(context.mutex);
+
+    void *data = rawstor_thread_join(thread);
+    assertTrue(data == &context);
+
+    assertTrue(context.value == 1);
+
+    return 0;
+}
+
+
+static int test_cond_broadcast() {
+    int count = 10;
+
+    struct TestThreadingContext context = (struct TestThreadingContext) {
+        .mutex = rawstor_mutex_create(),
+        .cond = rawstor_cond_create(),
+        .wait = 0,
+        .value = 0,
+    };
+
+    assertTrue(context.mutex != NULL);
+    assertTrue(context.cond != NULL);
+
+    RawstorThread **threads = calloc(count, sizeof(RawstorThread*));
+    assertTrue(threads != NULL);
+
+    for (int i = 0; i < count; ++i) {
+        threads[i] = rawstor_thread_create(test_thread, &context);
+    }
+
+    while (1) {
+        rawstor_mutex_lock(context.mutex);
+        int wait = context.wait;
+        rawstor_mutex_unlock(context.mutex);
+        if (wait == count) {
+            break;
+        }
+    }
+
+    rawstor_mutex_lock(context.mutex);
+    rawstor_cond_broadcast(context.cond);
+    rawstor_mutex_unlock(context.mutex);
+
+    for (int i = 0; i < count; ++i) {
+        void *data = rawstor_thread_join(threads[i]);
+        assertTrue(data == &context);
+    }
+
+    assertTrue(context.value == count);
+
+    return 0;
+}
+
+
+int main() {
+    int rval = 0;
+    rval += test_cond_signal();
+    rval += test_cond_broadcast();
+    return rval ? EXIT_FAILURE : EXIT_SUCCESS;
+}
