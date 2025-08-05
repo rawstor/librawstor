146bbd8554663d819d9f6c271263f7b026199320
vasily <vasily.stepanov@gmail.com>
2025-08-06 01:33:02 +0300

    add: publish fio results

---
+++ .github/tools/commit-and-push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+REPO=$1
+MESSAGE=$2
+
+pushd .
+cd librawstor
+COMMIT_AUTHOR=$(git log -1 --pretty=%an)
+COMMIT_EMAIL=$(git log -1 --pretty=%ae)
+popd
+
+git config --global user.name ${COMMIT_AUTHOR}
+git config --global user.email ${COMMIT_EMAIL}
+
+cd ${REPO}
+
+git add .
+git commit -m ${MESSAGE}
+git push
