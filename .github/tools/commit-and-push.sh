cf247810673d928183f6938156cf60ec706f1c84
vasily <vasily.stepanov@gmail.com>
2025-08-06 16:46:46 +0300

    add: quotas

--- .github/tools/commit-and-push.sh
+++ .github/tools/commit-and-push.sh
@@ -11,11 +11,11 @@ COMMIT_AUTHOR=$(git log -1 --pretty=%an)
 COMMIT_EMAIL=$(git log -1 --pretty=%ae)
 popd
 
-git config --global user.name ${COMMIT_AUTHOR}
-git config --global user.email ${COMMIT_EMAIL}
+git config --global user.name "${COMMIT_AUTHOR}"
+git config --global user.email "${COMMIT_EMAIL}"
 
 cd ${REPO}
 
 git add .
-git commit -m ${MESSAGE}
+git commit -m "${MESSAGE}"
 git push
