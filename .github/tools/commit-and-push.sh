83b1d33bcc2d1501ee33547f2b1b8bf5ddf1e143
vasily <vasily.stepanov@gmail.com>
2025-08-07 00:12:06 +0300

    fix: setup git before clone

--- .github/tools/commit-and-push.sh
+++ .github/tools/commit-and-push.sh
@@ -5,15 +5,6 @@ set -ex
 REPO=$1
 MESSAGE=$2
 
-pushd .
-cd librawstor
-COMMIT_AUTHOR=$(git log -1 --pretty=%an)
-COMMIT_EMAIL=$(git log -1 --pretty=%ae)
-popd
-
-git config --global user.name "${COMMIT_AUTHOR}"
-git config --global user.email "${COMMIT_EMAIL}"
-
 cd ${REPO}
 
 git add .
