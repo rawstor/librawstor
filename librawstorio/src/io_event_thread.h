33f8a44c32a060c7cb638405744b80a31eb74e07
vasily <vasily.stepanov@gmail.com>
2025-08-11 12:21:06 +0300

    ref: move event methods to io_event_thread

---
+++ librawstorio/src/io_event_thread.h
@@ -0,0 +1,43 @@
+#ifndef RAWSTORIO_IO_EVENT_THREAD_H
+#define RAWSTORIO_IO_EVENT_THREAD_H
+
+#include "io_session_thread.h"
+
+#include <rawstorstd/logging.h>
+
+#include <stdio.h>
+
+
+struct RawstorIOEvent {
+    RawstorIOSession *session;
+
+    struct iovec *iov_origin;
+    struct iovec *iov_at;
+    unsigned int niov_at;
+    off_t offset_at;
+    ssize_t (*process)(RawstorIOEvent *event);
+
+    RawstorIOCallback *callback;
+
+    size_t size;
+    ssize_t result;
+    int error;
+
+    void *data;
+
+#ifdef RAWSTOR_TRACE_EVENTS
+    void *trace_event;
+#endif
+};
+
+
+ssize_t rawstor_io_event_process_readv(RawstorIOEvent *event);
+
+ssize_t rawstor_io_event_process_preadv(RawstorIOEvent *event);
+
+ssize_t rawstor_io_event_process_writev(RawstorIOEvent *event);
+
+ssize_t rawstor_io_event_process_pwritev(RawstorIOEvent *event);
+
+
+#endif // RAWSTORIO_IO_EVENT_THREAD_H
