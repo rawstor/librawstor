6321a9857551ea5370c53a1f30d58fde53e3ce70
Vasily <vasily.stepanov@gmail.com>
2025-08-11 13:15:19 +0300

    ref: move io_event methods to io_event_poll.h (#34)
    I've refactored the codebase to improve modularity and organization. This pull request primarily focuses on relocating `io_event` related method definitions to a more appropriate source file. This change groups related functionalities together, making the code easier to navigate and maintain without altering any behavior.
    
    ### Highlights
    
    * **Function Relocation**: I have moved the definitions of several `rawstor_io_event_*` functions, including `rawstor_io_event_fd`, `rawstor_io_event_size`, `rawstor_io_event_result`, `rawstor_io_event_error`, and `rawstor_io_event_dispatch`, from `librawstorio/src/io_poll.c` to `librawstorio/src/io_event_poll.c`.
    * **Header Inclusion**: To support the moved functions, I have added the necessary `#include <rawstorio/io_event.h>` directive to `librawstorio/src/io_event_poll.c`.

--- librawstorio/src/io_event_poll.c
+++ librawstorio/src/io_event_poll.c
@@ -1,4 +1,5 @@
 #include "io_event_poll.h"
+#include <rawstorio/io_event.h>
 
 #include <sys/types.h>
 #include <sys/uio.h>
@@ -68,5 +69,38 @@ ssize_t rawstor_io_event_process_pwritev(RawstorIOEvent *event) {
     } else {
         event->result += ret;
     }
+    return ret;
+}
+
+
+int rawstor_io_event_fd(RawstorIOEvent *event) {
+    return event->session->fd;
+}
+
+
+size_t rawstor_io_event_size(RawstorIOEvent *event) {
+    return event->size;
+}
+
+
+size_t rawstor_io_event_result(RawstorIOEvent *event) {
+    return event->result;
+}
+
+
+int rawstor_io_event_error(RawstorIOEvent *event) {
+    return event->error;
+}
+
+
+int rawstor_io_event_dispatch(RawstorIOEvent *event) {
+#ifdef RAWSTOR_TRACE_EVENTS
+    rawstor_trace_event_message(event->trace_event, "dispatch()\n");
+#endif
+    int ret = event->callback(event, event->data);
+#ifdef RAWSTOR_TRACE_EVENTS
+    rawstor_trace_event_message(
+        event->trace_event, "dispatch(): rval = %d\n", ret);
+#endif
     return ret;
 }
