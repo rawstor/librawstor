4b94ff2bffa0ed131641f22059b67b435318f242
vasily <vasily.stepanov@gmail.com>
2025-08-11 15:14:36 +0300

    add: bulk io

--- librawstorio/src/io_thread.h
+++ librawstorio/src/io_thread.h
@@ -7,5 +7,8 @@
 
 int rawstor_io_push_cqe(RawstorIO *io, RawstorIOEvent *event);
 
+int rawstor_io_push_cqes(
+    RawstorIO *io, RawstorIOEvent **events, size_t nevents);
+
 
 #endif // RAWSTORIO_IO_THREAD_H
