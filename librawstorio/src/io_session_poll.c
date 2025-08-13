9bd5d3f748664034706b8242f01f53c861dc9bf3
vasily <vasily.stepanov@gmail.com>
2025-08-14 00:13:44 +0300

    wip: move push events logic in session

---
+++ librawstorio/src/io_session_poll.c
@@ -0,0 +1,110 @@
+#include "io_session_poll.h"
+
+#include "io_event_poll.h"
+
+#include <sys/uio.h>
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+RawstorIOSession* rawstor_io_session_create(int fd, int depth) {
+    RawstorIOSession *session = malloc(sizeof(RawstorIOSession));
+    if (session == NULL) {
+        goto err_session;
+    }
+
+    session->fd = fd;
+
+    session->read_events = rawstor_ringbuf_create(
+        depth, sizeof(RawstorIOEvent));
+    if (session->read_events == NULL) {
+        goto err_read_events;
+    }
+
+    session->write_events = rawstor_ringbuf_create(
+        depth, sizeof(RawstorIOEvent));
+    if (session->write_events == NULL) {
+        goto err_write_events;
+    }
+
+    return session;
+
+err_write_events:
+    rawstor_ringbuf_delete(session->read_events);
+err_read_events:
+    free(session);
+err_session:
+    return NULL;
+}
+
+
+void rawstor_io_session_delete(RawstorIOSession *session) {
+    rawstor_ringbuf_delete(session->read_events);
+    rawstor_ringbuf_delete(session->write_events);
+    free(session);
+}
+
+
+RawstorIOEvent* rawstor_io_session_push_read_event(
+    RawstorIOSession *session,
+    struct iovec *iov, unsigned int niov, size_t size, off_t offset,
+    RawstorIOCallback *cb, void *data)
+{
+    RawstorIOEvent *event = rawstor_ringbuf_head(session->read_events);
+    if (rawstor_ringbuf_push(session->read_events)) {
+        goto err_event;
+    }
+
+    *event = (RawstorIOEvent) {
+        .session = session,
+        .iov_origin = iov,
+        .iov_at = iov,
+        .niov = niov,
+        .offset = offset,
+        .callback = cb,
+        .size = size,
+        .result = 0,
+        // .error
+        .data = data,
+    };
+
+    return event;
+
+err_event:
+    return NULL;
+}
+
+
+RawstorIOEvent* rawstor_io_session_push_write_event(
+    RawstorIOSession *session,
+    struct iovec *iov, unsigned int niov, size_t size, off_t offset,
+    RawstorIOCallback *cb, void *data)
+{
+    RawstorIOEvent *event = rawstor_ringbuf_head(session->write_events);
+    if (rawstor_ringbuf_push(session->write_events)) {
+        goto err_event;
+    }
+
+    *event = (RawstorIOEvent) {
+        .session = session,
+        .iov_origin = iov,
+        .iov_at = iov,
+        .niov = niov,
+        .offset = offset,
+        // .process
+        .callback = cb,
+        .size = size,
+        .result = 0,
+        // .error
+        .data = data,
+    };
+
+    return event;
+
+err_event:
+    return NULL;
+}
+
+
--- librawstorio/src/io_session_poll.h
+++ librawstorio/src/io_session_poll.h
@@ -1,14 +1,36 @@
 #ifndef RAWSTORIO_IO_SESSION_POLL_H
 #define RAWSTORIO_IO_SESSION_POLL_H
 
+#include <rawstor/io.h>
+
 #include <rawstorstd/ringbuf.h>
+
+#include <sys/uio.h>
+
+#include <stddef.h>
+#include <stdio.h>
 
 
 typedef struct RawstorIOSession {
     int fd;
-    RawstorRingBuf *read_ops;
-    RawstorRingBuf *write_ops;
+    RawstorRingBuf *read_events;
+    RawstorRingBuf *write_events;
 } RawstorIOSession;
+
+
+RawstorIOSession* rawstor_io_session_create(int fd, int depth);
+
+void rawstor_io_session_delete(RawstorIOSession *session);
+
+RawstorIOEvent* rawstor_io_session_push_read_event(
+    RawstorIOSession *session,
+    struct iovec *iov, unsigned int niov, size_t size, off_t offset,
+    RawstorIOCallback *cb, void *data);
+
+RawstorIOEvent* rawstor_io_session_push_write_event(
+    RawstorIOSession *session,
+    struct iovec *iov, unsigned int niov, size_t size, off_t offset,
+    RawstorIOCallback *cb, void *data);
 
 
 #endif // RAWSTORIO_IO_SESSION_POLL_H
