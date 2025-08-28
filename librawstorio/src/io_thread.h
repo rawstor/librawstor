964aaf8d618d4d30171e3496a328a480a2b42127
vasily <vasily.stepanov@gmail.com>
2025-08-28 08:51:42 +0300

    add: cpp support

--- librawstorio/src/io_thread.h
+++ librawstorio/src/io_thread.h
@@ -5,10 +5,20 @@
 #include <rawstorio/io_event.h>
 
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
 int rawstor_io_push_cqe(RawstorIO *io, RawstorIOEvent *event);
 
 int rawstor_io_push_cqes(
     RawstorIO *io, RawstorIOEvent **events, size_t nevents);
+
+
+#ifdef __cplusplus
+}
+#endif
 
 
 #endif // RAWSTORIO_IO_THREAD_H
