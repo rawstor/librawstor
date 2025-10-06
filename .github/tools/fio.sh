a8293c22675277a5402d07be0d597ee65eb04387
vasily <vasily.stepanov@gmail.com>
2025-10-06 17:03:02 +0300

    fix: fio error

--- .github/tools/fio.sh
+++ .github/tools/fio.sh
@@ -44,6 +44,6 @@ while IFS='' read line; do
 done < ${_FIO_OUTPUT}
 
 cp ${_FIO_TXT} ${FIO_TXT}
-cp ${_FIO_JSON} ${FIO_JSON}
+cat ${_FIO_JSON} | sed s/\\\\:/:/g > ${FIO_JSON}
 
 rm ${_FIO_OUTPUT} ${_FIO_TXT} ${_FIO_JSON}
