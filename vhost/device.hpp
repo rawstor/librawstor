b5e51e6cf9d6d3b9bc3d2c6a4141baa7ed8cf98f
vasily <vasily.stepanov@gmail.com>
2025-11-07 00:39:54 +0300

    add: VHOST_USER_GET_CONFIG

--- vhost/device.hpp
+++ vhost/device.hpp
@@ -1,6 +1,7 @@
 #ifndef RAWSTOR_VHOST_DEVICE_HPP
 #define RAWSTOR_VHOST_DEVICE_HPP
 
+#include "protocol.h"
 #include "virtqueue.hpp"
 
 #include <rawstorstd/gpp.hpp>
@@ -20,15 +21,17 @@ class Device final {
     private:
         int _fd;
         int _backend_fd;
-        uint64_t _features;
+        uint64_t _protocol_features;
         std::vector<VirtQueue> _vq;
+        VirtioBlkConfig _config;
 
     public:
         explicit Device(int fd):
             _fd(fd),
             _backend_fd(-1),
-            _features(0),
-            _vq(1)
+            _protocol_features(0),
+            _vq(1),
+            _config{}
         {}
         Device(const Device &) = delete;
         Device(Device &&) = delete;
@@ -41,22 +44,12 @@ class Device final {
             return _fd;
         }
 
-        uint64_t get_features() const noexcept {
-            return _features;
-        }
+        uint64_t get_features() const noexcept;
 
-        void set_features(uint64_t features) noexcept {
-            _features = features;
-        }
+        uint64_t get_protocol_features() const noexcept;
 
-        uint64_t get_max_mem_slots() const noexcept {
-            /**
-             * vhost in the kernel usually supports 509 mem slots. 509 used to
-             * be the KVM limit, it supported 512, but 3 were used for internal
-             * purposes. This limit is sufficient to support many DIMMs and
-             * virtio-mem in "dynamic-memslots" mode.
-             */
-            return 509;
+        void set_protocol_features(uint64_t features) noexcept {
+            _protocol_features = features;
         }
 
         void set_backend_fd(int fd) {
@@ -75,6 +68,20 @@ class Device final {
         void set_vring_call(size_t index, int fd);
 
         void set_vring_err(size_t index, int fd);
+
+        const VirtioBlkConfig& get_config() const noexcept {
+            return _config;
+        }
+
+        uint64_t get_max_mem_slots() const noexcept {
+            /**
+             * vhost in the kernel usually supports 509 mem slots. 509 used to
+             * be the KVM limit, it supported 512, but 3 were used for internal
+             * purposes. This limit is sufficient to support many DIMMs and
+             * virtio-mem in "dynamic-memslots" mode.
+             */
+            return 509;
+        }
 
         void loop();
 };
