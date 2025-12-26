cdd050f4653a8789266639b8466ad04cde291dfc
vasily <vasily.stepanov@gmail.com>
2025-12-26 13:26:43 +0300

    fix: rawstor_object_id() signature

--- src/object.cpp
+++ src/object.cpp
@@ -473,7 +473,7 @@ int rawstor_object_close(RawstorObject *object) {
 }
 
 
-void rawstor_object_id(const RawstorObject *object, char *buf[37]) {
+void rawstor_object_id(const RawstorObject *object, char **buf) {
     rawstor_uuid_to_string(&object->id(), (RawstorUUIDString*)buf);
 }
 
