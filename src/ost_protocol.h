878c8e98cb24c0de4a3b1832cffb7540b53980bb
vasily <vasily.stepanov@gmail.com>
2025-12-30 15:20:25 +0300

    fix: linter issues

--- src/ost_protocol.h
+++ src/ost_protocol.h
@@ -7,14 +7,11 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
 #define RAWSTOR_MAGIC 0x72737472 // "rstr" as ascii
-
 
 enum RawstorOSTCommandType {
     RAWSTOR_CMD_SET_OBJECT,
@@ -23,52 +20,46 @@ enum RawstorOSTCommandType {
     RAWSTOR_CMD_DISCARD,
 };
 
-
 /* Just for basic validation only */
 struct RawstorOSTFrameCmdOnly {
-    uint32_t magic;
-    enum RawstorOSTCommandType cmd;
+        uint32_t magic;
+        enum RawstorOSTCommandType cmd;
 } RAWSTOR_PACKED;
-
 
 /* Minimalistic protocol frame */
 struct RawstorOSTFrameBasic {
-    uint32_t magic;
-    enum RawstorOSTCommandType cmd;
-    // var is for minimal commands only,
-    // will be overridden in other command structs
-    uint8_t obj_id[16];
-    uint64_t offset;
-    uint64_t val;
+        uint32_t magic;
+        enum RawstorOSTCommandType cmd;
+        // var is for minimal commands only,
+        // will be overridden in other command structs
+        uint8_t obj_id[16];
+        uint64_t offset;
+        uint64_t val;
 } RAWSTOR_PACKED;
-
 
 struct RawstorOSTFrameIO {
-    uint32_t magic;
-    enum RawstorOSTCommandType cmd;
-    uint16_t cid;
-    uint64_t offset;
-    uint32_t len;
-    uint64_t hash;
-    bool sync;
+        uint32_t magic;
+        enum RawstorOSTCommandType cmd;
+        uint16_t cid;
+        uint64_t offset;
+        uint32_t len;
+        uint64_t hash;
+        bool sync;
 } RAWSTOR_PACKED;
-
 
 /* response frames */
 struct RawstorOSTFrameResponse {
-    uint32_t magic;
-    enum RawstorOSTCommandType cmd;
-    uint16_t cid;
-    // TODO: if we send length in res - it should be the same type
-    // (signed-unsigned too)
-    int32_t res;
-    uint64_t hash;
+        uint32_t magic;
+        enum RawstorOSTCommandType cmd;
+        uint16_t cid;
+        // TODO: if we send length in res - it should be the same type
+        // (signed-unsigned too)
+        int32_t res;
+        uint64_t hash;
 } RAWSTOR_PACKED;
-
 
 #ifdef __cplusplus
 }
 #endif
-
 
 #endif // RAWSTOR_OST_PROTOCOL_H
