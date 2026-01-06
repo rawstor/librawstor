242b91a7072180044a1a6cb9475fb22e4fb5f0ac
vasily <vasily.stepanov@gmail.com>
2026-01-06 19:06:23 +0000

    fix: ignore SIGPIPE in librawstorio

--- librawstorio/src/queue.cpp
+++ librawstorio/src/queue.cpp
@@ -8,12 +8,36 @@
 #include "poll_queue.hpp"
 #endif
 
+#include <signal.h>
+
 #include <memory>
+
+namespace {
+
+extern void (*initialize)();
+
+void initialize_noop() {
+}
+
+void initialize_once() {
+    struct sigaction sa;
+    sa.sa_handler = SIG_IGN;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGPIPE, &sa, NULL);
+
+    initialize = initialize_noop;
+}
+
+void (*initialize)() = initialize_once;
+
+} // unnamed namespace
 
 namespace rawstor {
 namespace io {
 
 std::unique_ptr<Queue> Queue::create(unsigned int depth) {
+    initialize();
 #ifdef RAWSTOR_WITH_LIBURING
     return std::make_unique<rawstor::io::uring::Queue>(depth);
 #else
