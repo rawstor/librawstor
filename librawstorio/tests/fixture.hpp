960389b7f1f8aa14adc03da422beec406673f02b
vasily <vasily.stepanov@gmail.com>
2026-01-27 16:19:38 +0300

    fix: linter

--- librawstorio/tests/fixture.hpp
+++ librawstorio/tests/fixture.hpp
@@ -59,9 +59,7 @@ protected:
         _server(),
         _fd(_connect(_server.name())),
         _queue(rawstor::io::Queue::create(depth)) {}
-    ~QueueTest() {
-        close(_fd);
-    }
+    ~QueueTest() { close(_fd); }
 
     void _wait_all() {
         try {
