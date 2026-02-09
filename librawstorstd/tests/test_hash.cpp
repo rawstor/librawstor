4df93dc246fdebcf18a7593ec798f3ca433257c3
vasily <vasily.stepanov@gmail.com>
2026-02-09 13:10:17 +0300

    ref: remove unnecessary const_cast

--- librawstorstd/tests/test_hash.cpp
+++ librawstorstd/tests/test_hash.cpp
@@ -12,7 +12,7 @@ namespace {
 
 TEST(HashTest, scalar) {
     const char* buf = "hello world";
-    uint64_t hash = rawstor_hash_scalar(const_cast<char*>(buf), strlen(buf));
+    uint64_t hash = rawstor_hash_scalar(buf, strlen(buf));
 #ifdef RAWSTOR_WITH_LIBXXHASH
     EXPECT_EQ(hash, 0xd447b1ea40e6988b);
 #else
@@ -24,7 +24,7 @@ TEST(HashTest, vector) {
     const char* s1 = "hello";
     const char* s2 = " ";
     const char* s3 = "world";
-    iovec iov[] = {
+    const iovec iov[] = {
         {
             .iov_base = const_cast<char*>(s1),
             .iov_len = strlen(s1),
