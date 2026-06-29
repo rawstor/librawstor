5b5cb4c79e3f6a0aeabef2c52c982ced40a753fa
vasily <vasily.stepanov@gmail.com>
2026-06-29 13:57:27 +0300

    wip: python bindings

---
+++ pyrawstor/rawstor/module.c
@@ -0,0 +1,18 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+static struct PyModuleDef rawstor_module = {
+    PyModuleDef_HEAD_INIT,
+    "rawstor",
+    NULL,
+    -1,
+};
+
+PyMODINIT_FUNC PyInit_rawstor() {
+    PyObject *m = PyModule_Create(&rawstor_module);
+    if (m == NULL) {
+        return NULL;
+    }
+
+    return m;
+}
