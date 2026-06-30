bbd66fbb4a83c8f87ecd91d924f455be20fd1900
vasily <vasily.stepanov@gmail.com>
2026-06-30 12:48:11 +0300

    fix: remove tests from rawstor package

--- pyrawstor/setup.py
+++ pyrawstor/setup.py
@@ -80,7 +80,6 @@ if __name__ == "__main__":
         version=config["package_version"],
         packages=[
             "rawstor",
-            "tests",
         ],
         ext_modules=extensions,
     )
