789b6c4bf62768c6c46ee32778e98a98d96cc71f
vasily <vasily.stepanov@gmail.com>
2026-07-01 17:58:45 +0300

    add: initialize and terminate bindings

--- pyrawstor/rawstor/module.c
+++ pyrawstor/rawstor/module.c
@@ -1,11 +1,24 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
+#include "rawstor_bindings.h"
+
+static PyMethodDef rawstor_methods[] = {
+    {"initialize", py_rawstor_initialize, METH_VARARGS, NULL},
+    {"terminate", py_rawstor_terminate, METH_NOARGS, NULL},
+    {NULL, NULL, 0, NULL}
+};
+
 static struct PyModuleDef rawstor_module = {
-    PyModuleDef_HEAD_INIT,
-    "rawstor",
-    NULL,
-    -1,
+    .m_base = PyModuleDef_HEAD_INIT,
+    .m_name = "rawstor",
+    .m_doc = NULL,
+    .m_size = -1,
+    .m_methods = rawstor_methods,
+    .m_slots = NULL,
+    .m_traverse = NULL,
+    .m_clear = NULL,
+    .m_free = NULL,
 };
 
 PyMODINIT_FUNC PyInit_rawstor() {
