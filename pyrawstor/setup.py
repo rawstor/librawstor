a1d9946f7f4604434cc4c70cbdbbdda39f5baca4
vasily <vasily.stepanov@gmail.com>
2026-07-02 15:27:47 +0300

    add: fPIC

--- pyrawstor/setup.py
+++ pyrawstor/setup.py
@@ -70,6 +70,7 @@ if __name__ == "__main__":
                     "-Wall",
                     "-Wextra",
                     "-Werror",
+                    "-fPIC",
                 ],
             ),
         ],
