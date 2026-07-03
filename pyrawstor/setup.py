65a32abc223a637459446c8cfdc050c7c9d128ad
vasily <vasily.stepanov@gmail.com>
2026-07-03 11:22:15 +0300

    fix: cleanup

--- pyrawstor/setup.py
+++ pyrawstor/setup.py
@@ -13,31 +13,6 @@ def get_config():
         exec(f.read(), ret)
     return ret
 
-def call_pkg_config(module, *args):
-    return check_output(["pkg-config", module, *args]).decode("utf-8")[:-1]
-
-
-def pkg_config_include_dirs(module):
-    includes = filter(
-        lambda lib: lib,
-        call_pkg_config(module, "--cflags-only-I").split(" "))
-    return [include[2:] for include in includes]
-
-
-def pkg_config_lib_dirs(module):
-    libs = filter(
-        lambda lib: lib,
-        call_pkg_config(module, "--libs-only-L").split(" "))
-    return [lib[2:] for lib in libs]
-
-
-def pkg_config_libs(module):
-    libs = filter(
-        lambda lib: lib,
-        call_pkg_config(module, "--libs-only-l").split(" "))
-    return [lib[2:] for lib in libs]
-
-
 if __name__ == "__main__":
     config = get_config()
 
