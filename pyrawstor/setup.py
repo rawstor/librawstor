d3e6356ca21ca788c5cac2d53a9582d8b82a040e
vasily <vasily.stepanov@gmail.com>
2026-07-03 14:06:02 +0300

    del: runtime_library_dirs

--- pyrawstor/setup.py
+++ pyrawstor/setup.py
@@ -46,9 +46,6 @@ if __name__ == "__main__":
                 ],
                 extra_compile_args=os.getenv('CFLAGS', '').split(),
                 extra_link_args=os.getenv('LDFLAGS', '').split(),
-                runtime_library_dirs=[
-                    "../src/.libs"
-                ],
             ),
         ],
     )
