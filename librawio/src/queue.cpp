6b97a721cd16abefca357939b7fcd1e93a861869
vasily <vasily.stepanov@gmail.com>
2026-08-06 09:35:26 +0000

    ref: suppress SIGPIPE via MSG_NOSIGNAL instead of a global ignore
    OST client/server writes and the rawstor-vhost control-socket write now
    pass MSG_NOSIGNAL (SO_NOSIGPIPE on macOS) instead of relying on the
    process-wide sigaction(SIGPIPE, SIG_IGN) librawio used to install for
    every rawstor binary. rawstor-vhost-qemu keeps a local SIGPIPE ignore
    since the vendored libvhost-user writes to its control socket without
    MSG_NOSIGNAL and can't be patched.

--- librawio/src/queue.cpp
+++ librawio/src/queue.cpp
@@ -8,24 +8,7 @@
 #include "poll_queue.hpp"
 #endif
 
-#include <signal.h>
-
 #include <memory>
-#include <mutex>
-
-namespace {
-
-std::once_flag initialize_once_flag;
-
-void initialize() {
-    struct sigaction sa;
-    sa.sa_handler = SIG_IGN;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    sigaction(SIGPIPE, &sa, nullptr);
-}
-
-} // unnamed namespace
 
 namespace rawio {
 
@@ -33,7 +16,6 @@ Queue::Queue(unsigned int depth) : _depth(depth) {
 }
 
 std::unique_ptr<Queue> Queue::create(unsigned int depth) {
-    std::call_once(initialize_once_flag, initialize);
 #ifdef RAWIO_WITH_LIBURING
     return std::make_unique<rawio::uring::Queue>(depth);
 #else
